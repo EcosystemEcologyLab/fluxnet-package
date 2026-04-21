356befc4d62069e4b7f7402c798f9dc6986ad7c3
Eric Scott <scottericr@gmail.com>
2026-04-21 16:35:11 -0700

    use fluxnet_shuttle python function for downloading

---
+++ tests/testthat/test-flux_download.R
@@ -0,0 +1,22 @@
+test_that("download of all files works", {
+  skip_on_ci()
+  download_dir <- withr::local_tempdir()
+  snapshot <- flux_listall()
+  out <- flux_download(
+    file_list_df = head(snapshot, n = 2),
+    download_dir = download_dir
+  )
+  expect_equal(nrow(out), 2)
+})
+
+test_that("download of specified site_ids works", {
+  skip_on_ci()
+  download_dir <- withr::local_tempdir()
+  snapshot <- flux_listall()
+  out <- flux_download(
+    file_list_df = snapshot,
+    site_ids = c("NZ-ScF"),
+    download_dir = download_dir
+  )
+  expect_equal(nrow(out), 1)
+})
