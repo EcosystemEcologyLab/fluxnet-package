8bcd70e489096419188dde72cf5406e73cba5b25
Eric Scott <scottericr@gmail.com>
2026-05-22 16:15:52 -0700

    fix setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,10 +1,12 @@
 # Download example sites for tests if they aren't there already
+fs::dir_create(testthat::test_path("testdata"))
 zips <- fs::dir_ls(testthat::test_path("testdata")) %>% fs::path_file()
 if (
-  !all(stringr::str_detect(
-    zips,
-    "(AMF_US-MMS_FLUXNET_)|(TERN_AU-Wom_FLUXNET_)"
-  ))
+  length(zips) == 0 |
+    !all(stringr::str_detect(
+      zips,
+      "(AMF_US-MMS_FLUXNET_)|(TERN_AU-Wom_FLUXNET_)"
+    ))
 ) {
   flux_download(
     site_ids = c("US-MMS", "AU-Wom"),
